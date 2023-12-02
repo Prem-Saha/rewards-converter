8f4f139ce1f132a4aeee7f18cb337af9bc3dc023
Prem Saha <91550890+Prem-Saha@users.noreply.github.com>
2023-12-02 21:13:13 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RewardValueTests {
 
     public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
@@ -5,11 +9,19 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-@@ -20,11 +22,18 @@ void create_with_miles_value() {
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
         double cashValue = 10;
         int expectedValue = (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
@@ -18,7 +30,6 @@ public class RewardValueTests {
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
         int milesValue = 1000;
         double expectedCash = milesValue * MILES_TO_CASH_CONVERSION_RATE;
         var rewardValue = new RewardValue(milesValue);
